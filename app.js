61986d395e1cb183513c45cf561ac0d6be4d2069
sang <sang@gmail.com>
2021-08-22 03:23:40 +0700

    sgame

---
+++ app.js
@@ -0,0 +1,10 @@
+
+
+// const imgcard=document.querySelector(".image--Card")
+// let parentcard=imgcard.parentElement.querySelector('.silder')
+// imgcard.onmouseover=()=>{
+//     parentcard.classList.add("sliderTobottom")
+// }
+// imgcard.onmouseout=()=>{
+//     parentcard.classList.remove("sliderTobottom")
+// }
